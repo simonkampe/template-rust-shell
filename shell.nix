c305c68187ec50aeac5dca736d035a401cd41738
Simon Kämpe <simon@linetic.se>
2022-12-16 15:09:49 +0100

    Add dependencies

--- shell.nix
+++ shell.nix
@@ -11,5 +11,20 @@ in pkgs.mkShell {
     pkg-config
     rust
     rust-analyzer
+
+    # Build deps
+    zlib
+    openssl
+    pkg-config
+    cmake
+
+    # GUI
+    fontconfig
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-headers
+    vulkan-loader
   ];
 }
